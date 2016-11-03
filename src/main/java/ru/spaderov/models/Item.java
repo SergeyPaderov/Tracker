f2e8b44e14e1aea90b72fd7b092ac5c0f8244b90
spov77@gmail.com <mmtrkqjz711>
2016-11-03 20:25:28 -0200

    Add method comment

--- src/main/java/ru/spaderov/models/Item.java
+++ src/main/java/ru/spaderov/models/Item.java
@@ -4,30 +4,33 @@ import java.lang.*;
 
 public class Item {
 	private String id;
-	
+
 	public String name;
-	
+
 	public String description;
-	
+
+	private String comment;
+
 	public long create;
-	
-	public Item() {		
+
+	public Item() {
 	}
-	
-	public Item(String name, String description, long create) {
+
+	public Item(String name, String description, long create, String comment) {
 		this.name = name;
 		this.description = description;
 		this.create = create;
-	} 
-	
+		this.comment = comment;
+	}
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public String getDescription() {
 		return this.description;
 	}
-	
+
 	public long getCreate() {
 		return this.create;
 	}
@@ -35,8 +38,12 @@ public class Item {
 	public String getId() {
 		return this.id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-} 
+
+	public void setConnect(String connect) {
+		this.comment = connect;
+	}
+}
